33d647f17ed3ab93c352ef0f0424b068f8a3bc43
Marc Matias <mrcallenmatias@gmail.com>
2022-02-27 22:21:52 -0800

    completed assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,60 +10,102 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
-
-
-// b) Create the function that makes the test pass.
-
-
-
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+// create a test
+describe("divByThree", () => {
+    const num1 = 15
+    const num2 = 0
+    const num3 = -7
 
+    //describe the output of the function
+    it("takes a number as an argument and return if the number is evenly divisble by three or not", () => {
+        // shows the result of the output
+        expect(divByThree(num1)).toEqual("15 is divisible by three")
+        expect(divByThree(num2)).toEqual("0 is divisible by three")
+        expect(divByThree(num3)).toEqual("-7 is not divisible by three")
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+// create a funtion name divByThree
+// adds a parameter with desire name
+// make an if statement and use (% 3) to find whether th number is divisible by 3
+// then return if the number is evenly divisble by three or not
 
+const divByThree = (array) => {
+    if (array % 3 === 0) {
+        return `${array} is divisible by three`
+    } else {
+        return `${array} is not divisible by three`
+    }
+}
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("arrayOfWords", () => {
+    const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+    const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
-
+    //describe the output of the function
+    it("takes in an array of words and returns an array with all the words capitalized", () => {
+        // shows the result of the output
+        expect(arrayOfWords(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(arrayOfWords(randomNouns2)).toEqual( ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
 // b) Create the function that makes the test pass.
 
+// create a function called arrayOfWords that takes in an array
+// use the map method to select the first letter of each word
+// capitalize the letter
+// use slice methode to add the rest of the letter that dont need to be capitalized
+// returns an array with all the words capitalized
+
+const arrayOfWords = (array) => {
+    return array.map(a => {
+        return a[0].toUpperCase() + a.slice(1)
+    })
+}
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+// Create a test with expect statements for each of the variables.
+describe("vowelIndex", () => {
+    const vowelTester1 = "learn"
+    // Expected output: 1
+    const vowelTester2 = "academy"
+    // Expected output: 0
+    const vowelTester3 = "challenges"
+    // Expected output: 2
 
+    //describe the output of the function
+    it("takes in a string and logs the index of the first vowel", () => {
+        // shows the result of the output
+        expect(vowelIndex(vowelTester1)).toEqual(1)
+        expect(vowelIndex(vowelTester2)).toEqual(0)
+        expect(vowelIndex(vowelTester3)).toEqual(2)
+    })
+})
 
-// b) Create the function that makes the test pass.
+// b) Create the function that makes the test pass.    
+
+// crete a function called vowelIndex that takes on string parameter
+// iterate through each string to go through the word letter bu letter
+// use if statement to find the vowel in the word
+// return the index of the first vowel
+
+const vowelIndex = (string) => {
+    for (i = 0; i < string.length; i++){
+        if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u")
+        return i
+    }   
+}         
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 var cohort = "Alpha 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ['A', 'l', 'p', 'h', 'a', ' ', '2', '0', '2', '2']
+// b) Verify and explain: Correct, split turns string into an array. Without any space or indication of how you wanna split the word into an array, it will split into that output.
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ var cohort = "Alpha 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined
+// b) Verify and explain: Correct, every function should have a return inside is. Without it, it will show undefined as the result.
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [ 8, 10, 12, 14, 16 ]
+// b) Verify and explain: Correct, by using map method, it will go through each number in an array and follow whatever command you put in. In this case, all numbers inside the array was multiplied by 2.
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[ 11, 13, 15 ]
+// b) Verify and explain: Correct, The filter method is use for array that goes through the data inside an array and filter whatever command you input.
 
 
 // --------------------5) What will this log?
@@ -51,10 +51,10 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: Correct, To find an array inside the object, you would first have to select the object, then select the object inside the object, once you have access to that object, you would have to use the method to find the index inside the array.
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +67,7 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: 'George', cohort: 'Alpha', year: 2022 }
+// b) Verify and explain: Correct, in the constructor inside the class method, it only has 1 parameter which is the name. When the name George was put inside the parameter in the variable learnStudent, it got transferred over inside the constructore after console logging it.
